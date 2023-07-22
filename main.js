4f828bdea2ea7dada2e281334e260fbe9159cb51
unknown <ahmedfathy241110@gmail.com>
2023-07-22 23:03:18 +0300

    aflamak

---
+++ main.js
@@ -0,0 +1,20 @@
+// let dataCard;
+// let romamticeSec = document.getElementById("auto-romantice");
+
+// fetch("card-data.json")
+//   .then((result) => {
+//     dataCard = result.json();
+//     return dataCard;
+//   })
+//   .then((result) => {
+//     for (let i = 0; i <= result.length; i++) {
+//       cardRomantic = `
+//             <div class="romantice-card">
+//             <a href="#">
+//               <img src="${result[i].image}" alt="" />
+//             </a>
+//           </div>
+//           `;
+//       romamticeSec.innerHTML += cardRomantic;
+//     }
+//   });
